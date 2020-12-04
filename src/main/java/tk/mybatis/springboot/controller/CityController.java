8a225236dacc0111af9a8e0e4a0eb11e0aefaa78
lawnstein.chan <lawnstein@126.com>
2020-12-04 11:11:14 +0800

    upgrade for tk version

--- src/main/java/tk/mybatis/springboot/controller/CityController.java
+++ src/main/java/tk/mybatis/springboot/controller/CityController.java
@@ -3,38 +3,36 @@
  *
  * Copyright (c) 2014-2016 abel533@gmail.com
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package tk.mybatis.springboot.controller;
 
-import com.github.pagehelper.PageInfo;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.github.pagehelper.PageInfo;
+
 import tk.mybatis.springboot.model.City;
 import tk.mybatis.springboot.service.CityService;
-
-import java.util.List;
 
 /**
  * @author liuzh
@@ -43,43 +41,47 @@ import java.util.List;
 @RestController
 @RequestMapping("/cities")
 public class CityController {
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    private CityService cityService;
+	@Autowired
+	private CityService cityService;
 
-    @RequestMapping
-    public PageInfo<City> getAll(City city) {
-        List<City> countryList = cityService.getAll(city);
-        return new PageInfo<City>(countryList);
-    }
+	@RequestMapping
+	public PageInfo<City> getAll(@RequestBody City city) {
+		LOG.debug("input.city: {}", city);
+		List<City> countryList = cityService.getAll(city);
+		LOG.debug("countryList: {}", countryList);
+		return new PageInfo<City>(countryList);
+	}
 
-    @RequestMapping(value = "/add")
-    public City add() {
-        return new City();
-    }
+	@RequestMapping(value = "/add")
+	public City add() {
+		return new City();
+	}
 
-    @RequestMapping(value = "/view/{id}")
-    public City view(@PathVariable Integer id) {
-        ModelAndView result = new ModelAndView();
-        City city = cityService.getById(id);
-        return city;
-    }
+	@RequestMapping(value = "/view/{id}")
+	public City view(@PathVariable Integer id) {
+		ModelAndView result = new ModelAndView();
+		City city = cityService.getById(id);
+		return city;
+	}
 
-    @RequestMapping(value = "/delete/{id}")
-    public ModelMap delete(@PathVariable Integer id) {
-        ModelMap result = new ModelMap();
-        cityService.deleteById(id);
-        result.put("msg", "删除成功!");
-        return result;
-    }
+	@RequestMapping(value = "/delete/{id}")
+	public ModelMap delete(@PathVariable Integer id) {
+		ModelMap result = new ModelMap();
+		cityService.deleteById(id);
+		result.put("msg", "删除成功!");
+		return result;
+	}
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public ModelMap save(City city) {
-        ModelMap result = new ModelMap();
-        String msg = city.getId() == null ? "新增成功!" : "更新成功!";
-        cityService.save(city);
-        result.put("city", city);
-        result.put("msg", msg);
-        return result;
-    }
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelMap save(@RequestBody City city) {
+		LOG.debug("input.city: {}", city);
+		ModelMap result = new ModelMap();
+		String msg = city.getId() == null ? "新增成功!" : "更新成功!";
+		cityService.save(city);
+		result.put("city", city);
+		result.put("msg", msg);
+		return result;
+	}
 }
